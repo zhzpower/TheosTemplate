3592355cff53c951508dbda776fe80c69d6fa9b0
zhz <zhaohuazhao@yupaopao.cn>
2022-06-12 09:25:37 +0800

    Initial Commit

---
+++ TheosTempelete/TheosTempelete.h
@@ -0,0 +1,12 @@
+//
+//  TheosTempelete.h
+//  TheosTempelete
+//
+//  Created by zhz on 2022/6/12.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TheosTempelete : NSObject
+
+@end
---
+++ TheosTempelete/TheosTempelete.m
@@ -0,0 +1,12 @@
+//
+//  TheosTempelete.m
+//  TheosTempelete
+//
+//  Created by zhz on 2022/6/12.
+//
+
+#import "TheosTempelete.h"
+
+@implementation TheosTempelete
+
+@end
